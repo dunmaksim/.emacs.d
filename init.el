bce3a291dc72045df67a0cc4ba90103a6223491e
Maxim Dunaevsky <dunmaksim@yandex.ru>
2018-01-30 20:50:51 +0300

    Removed anaconda, yasnippet and some other packages, added elpy

--- init.el
+++ init.el
@@ -139,16 +139,15 @@
 
 (use-package all-the-icons)
 
-(use-package anaconda-mode
-  :hook ((python-mode-hook . anaconda-mode)
-         (python-mode-hook . anaconda-eldoc-mode)))
+;; (use-package anaconda-mode
+;;   :mode "\\.py\\'"
+;;   :hook ((python-mode-hook . anaconda-mode)
+;;          (python-mode-hook . anaconda-eldoc-mode)))
 
 (use-package beacon
-  :diminish
   :commands beacon-mode)
 
 (use-package company
-  :diminish
   :commands company-mode
   :config
   ;; From https://github.com/company-mode/company-mode/issues/87
@@ -173,22 +172,14 @@
                 :filter (lambda (&optional _)
                           (when (check-expansion)
                             #'company-complete-common))))
+  ;; :init
+  (global-company-mode t))
 
-  (eval-after-load "yasnippet"
-    '(progn
-       (defun company-mode/backend-with-yas (backend)
-         (if (and (listp backend) (member 'company-yasnippet backend))
-             backend
-           (append (if (consp backend) backend (list backend))
-                   '(:with company-yasnippet))))
-       (setq company-backends
-             (mapcar #'company-mode/backend-with-yas company-backends))))
-  :init (global-company-mode t))
-
-(use-package company-anaconda
-  :requires anaconda-mode
-  :config
-  (add-to-list 'company-backends '(company-anaconda :with company-capf)))
+;; (use-package company-anaconda
+;;   :requires (company anaconda-mode)
+;;   :after anaconda-mode
+;;   :config
+;;   (add-to-list 'company-backends '(company-anaconda :with company-capf)))
 
 (use-package company-quickhelp
   :bind
@@ -198,11 +189,22 @@
 (use-package css-mode
   :mode "\\.css\\'")
 
+(use-package edit-server
+  :if window-system
+  :init
+  (add-hook 'after-init-hook 'server-start t)
+  (add-hook 'after-init-hook 'edit-server-start t))
+
 ;; (use-package elpy
-;;   :mode "\\.py\\'"
-;;   :requires python-mode
-;;   :hook (add-hook 'before-save-hook #'elpy-format-code)
+;;   :requires (company
+;;              py-isort
+;;              python-mode)
 ;;   :init
+;;   (add-hook 'before-save-hook 'elpy-format-code)
+;;   (add-hook 'before-save-hook 'py-isort-before-save)
+;;   :config
+;;   (python-mode)
+;;   (add-to-list 'company-backends 'elpy-company-backend)
 ;;   (elpy-enable)
 ;;   (defalias 'workon 'pyvenv-workon))
 
@@ -219,7 +221,8 @@
   :bind ([f10] . helm-buffers-list))
 
 (use-package highlight-numbers
-  :hook(prog-mode . highlight-numbers-mode))
+  :init(add-hook 'prog-mode-hook 'highlight-numbers-mode))
+;; :hook(prog-mode . highlight-numbers-mode))
 
 (use-package ibuffer
   :bind([f2] . ibuffer)
@@ -245,7 +248,9 @@
          ("\\.jshintrc\\'" . json-mode)))
 
 (use-package json-reformat
-  :after json-mode)
+  :requires json-mode)
+
+(global-linum-mode t)
 
 (use-package magit
   :bind([f5] . magit-status))
@@ -267,17 +272,29 @@
 
 (use-package powerline)
 
-(use-package py-isort
-  :hook
-  (add-hook 'before-save-hook 'py-isort-before-save))
-
 (use-package python-mode
-  :mode "\\.py\\'"
-  :interpreter ("python" . python-mode))
+  :requires (company)
+  :mode ("\\.py\\'" . python-mode)
+  :config
+  (use-package elpy
+    :config
+    (add-to-list 'company-backends 'elpy-company-backend)
+    (elpy-enable)
+    (defalias 'workon 'pyvenv-workon))
+  (use-package py-autopep8
+    :hook
+    (python-mode . py-autopep8-enable-on-save))
+  (use-package py-isort
+    :init
+    (add-hook 'before-save-hook #'py-isort-before-save)))
 
 (use-package rainbow-delimiters
+  :commands rainbow-delimiters-mode
   :hook
-  (prog-mode . rainbow-delimiters-mode))
+  (prog-mode . rainbow-delimiters-mode)
+  :config
+  (show-paren-mode 1)
+  (electric-pair-mode 1))
 
 (scroll-bar-mode -1)
 
@@ -318,44 +335,6 @@
   (with-eval-after-load 'flycheck
     (flycheck-add-mode 'typescript-tslint 'web-mode)))
 
-;; (use-package tide
-;;   :commands tide-setup tide-mode setup-tide-mode
-;;   :config
-;;   (defun setup-tide-mode ()
-;;     (interactive)
-;;     ;;(require 'typescript-mode)
-;;     (tide-setup)
-;;     (tide-mode +1)
-;;     (flycheck-mode +1)
-;;     (setq-default tab-width 2)
-;;     (setq flycheck-check-syntax-automatically '(save mode-enabled))
-;;     (eldoc-mode +1)
-;;     (tide-hl-identifier-mode +1)
-;;     (company-mode +1))
-;;   (require 'typescript-mode)
-;;   (add-hook 'typescript-mode-hook #'setup-tide-mode)
-
-;;   ;; aligns annotation to the right hand side
-;;   (setq company-tooltip-align-annotations t)
-;;   ;; format options
-;;   ;; (setq tide-format-options '(:insertSpaceAfterFunctionKeywordForAnonymousFunctions t :placeOpenBraceOnNewLineForFunctions nil))
-;;   ;; support for JS files
-;;   (add-hook 'js2-mode-hook #'setup-tide-mode)
-;;   ;; from now on web-mode is required
-;;   (require 'web-mode)
-;;   ;; support for TSX files
-;;   (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
-;;   (add-hook 'web-mode-hook
-;;             (lambda ()
-;;               (when (string-equal "tsx" (file-name-extension buffer-file-name))
-;;                 (setup-tide-mode))))
-;;   ;; support for JSX files
-;;   (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
-;;   (add-hook 'web-mode-hook
-;;             (lambda ()
-;;               (when (string-equal "jsx" (file-name-extension buffer-file-name))
-;;                 (setup-tide-mode)))))
-
 (use-package typescript-mode
   :commands typescript-mode)
 
@@ -363,21 +342,19 @@
 
 (use-package web-mode
   :commands web-mode
-  :requires web-beautify
   :mode(("\\.phtml\\'" . web-mode)
         ("\\.html\\'" . web-mode))
   :hook (add-hook 'before-save-hook 'web-beautify-html-buffer t t))
 
-(use-package yasnippet
-  :after prog-mode
-  :defer 10
-  :diminish yas-minor-mode
-  :mode("/\\.emacs\\.d/snippets/" . snippet-mode)
-  :config
-  (yas-load-directory (expand-file-name "snippets" user-emacs-directory))
-  (yas-global-mode 1))
+;; (use-package yasnippet
+;;   :requires prog-mode
+;;   :defer 10
+;;   :mode("/\\.emacs\\.d/snippets/" . snippet-mode)
+;;   :config
+;;   (yas-load-directory (expand-file-name "snippets" user-emacs-directory))
+;;   (yas-global-mode 1))
 
-(use-package yasnippet-snippets
-  :after yasnippet)
+;; (use-package yasnippet-snippets
+;;   :requires yasnippet)
 
 ;;; init.el ends here

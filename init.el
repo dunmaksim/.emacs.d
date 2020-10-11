307fc6c08425e36ad24c382c6070a1a3dd45fb18
Dunaevsky Maxim <dunaevsky@outlook.com>
2020-10-11 17:35:24 +0300

    Updated settings for tide-mode

--- init.el
+++ init.el
@@ -355,23 +355,14 @@
 
 (scroll-bar-mode -1)
 
+
 (use-package tide
-  :config
-  (
-  (tyde-setup)
-  (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (eldoc-mode +1)
-  (tide-hl-identifier-mode +1)
-  ;; company is an optional dependency. You have to
-  ;; install it separately via package-install
-  ;; `M-x package-install [ret] company`
-  (company-mode +1))
-  :hook (
-         (before-save-hook . tide-format-before-save)
-;;         (typescript-mode-hook  . typescript-mode-hook-hook)
-         )
-  )
+  ;;; From https://github.com/ananthakumaran/tide
+  :ensure t
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode)
+         (before-save . tide-format-before-save)))
 
 
 (use-package typescript-mode
